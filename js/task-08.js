197ded9d0404a15a683da231b5695a417619323e
bestga84 <bestga84@gmail.com>
2023-12-20 19:53:35 +0200

    task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+const inputEvent = (event) => {
+    event.preventDefault();
+
+    const elements = {
+        mail,
+        password,
+    }
+
+    if (elements.mail.value === "" || elements.password.value === "") {
+        return alert("Please, try again.")
+    }
+    return event.currentTarget.reset();
+}
+
+
+
+loginForm.addEventListener("submit", inputEvent)
