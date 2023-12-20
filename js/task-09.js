dea085a53a7531354b1ab4066bc26b8d4be5ca2a
bestga84 <bestga84@gmail.com>
2023-12-20 20:24:39 +0200

    task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,23 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const mainColor = document.querySelector("body");
+const colors = {
+  btnChangeColor: document.querySelector(".change-color"),
+  color: document.querySelector(".color"),
+  backColor: rgb(255, 255, 255),
+}
+
+mainColor.style.backgroundColor = colors.backColor;
+
+colors.textContent = colors.backColor;
+
+const changeBackColor = (event) => {
+  const colorRandom = getRandomHexColor();
+  mainColor.style.backgroundColor = colorRandom;
+  colors.color.textContent = colorRandom;
+};
+
+colors.btnChangeColor.addEventListener("click", changeBackColor);
