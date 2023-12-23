d54536a4b12de8bd0729ef9727c4a27520161fb9
bestga84 <bestga84@gmail.com>
2023-12-23 21:01:42 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,17 @@
 const validInputEl = document.querySelector("#validation-input");
 
 const InputElLength = validInputEl.dataset.length;
-const expectedLength = parseInt(validInputEl.getAttribute("data-length"));
 
 validInputEl.addEventListener("blur", (event) => {
-    if (event.currentTarget.expectedLength.length >= InputElLength) {
-        return validInputEl.classList.add("valid");
-    } 
-    return validInputEl.classList.add("invalid");
+    if (event.currentTarget.value.length == InputElLength) {
+        validInputEl.classList.add("valid");
+        validInputEl.classList.remove("invalid");
+    } else {
+        validInputEl.classList.add("invalid");
+        validInputEl.classList.add("valid");
+    }
 });
 
-
-
-console.log(validInputEl);
-console.log(InputElLength);
-console.log(expectedLength);
+// console.log(validInputEl);
+// console.log(InputElLength);
+// console.log(expectedLength);
